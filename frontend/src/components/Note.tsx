fdd64e6f458487681b4237a3fd934db5cefb0269
crystalzipzap <cschmidt@ou.edu>
2024-01-15 22:28:23 -0600

    Frontend create call

---
+++ frontend/src/components/Note.tsx
@@ -0,0 +1,35 @@
+import styles from "../styles/Note.module.css";
+import { Card } from "react-bootstrap";
+import { Note as NoteModel } from "../models/note";
+import { formatDate } from "../utils/FormatDate";
+
+interface NoteProps {
+  note: NoteModel;
+  className?: string;
+}
+
+const Note = ({ note, className }: NoteProps) => {
+  const { title, text, createdAt, updatedAt } = note;
+
+  let createdUpdatedText: string;
+
+  if (updatedAt > createdAt) {
+    createdUpdatedText = `Updated: ${formatDate(updatedAt)}`;
+  } else if (updatedAt <= createdAt) {
+    createdUpdatedText = `Created: ${formatDate(createdAt)}`;
+  } else {
+    createdUpdatedText = `Updated: ${formatDate(updatedAt)}`;
+  }
+
+  return (
+    <Card className={`${styles.noteCard} ${className}`}>
+      <Card.Body className={styles.cardBody}>
+        <Card.Title>{title}</Card.Title>
+        <Card.Text className={styles.cardText}>{text}</Card.Text>
+      </Card.Body>
+      <Card.Footer className="text-muted">{createdUpdatedText}</Card.Footer>
+    </Card>
+  );
+};
+
+export default Note;
