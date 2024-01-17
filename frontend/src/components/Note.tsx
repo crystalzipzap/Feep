c8568e9751e8e9a5f7ac2fd0156f4e6476814d77
crystalzipzap <cschmidt@ou.edu>
2024-01-16 22:54:26 -0600

    Frontend error handling

--- frontend/src/components/Note.tsx
+++ frontend/src/components/Note.tsx
@@ -1,14 +1,24 @@
 import styles from "../styles/Note.module.css";
+import stylesUtils from "../styles/utils.module.css";
 import { Card } from "react-bootstrap";
 import { Note as NoteModel } from "../models/note";
 import { formatDate } from "../utils/FormatDate";
+import { MdDelete } from "react-icons/md";
 
+//hoisting -
 interface NoteProps {
   note: NoteModel;
   className?: string;
+  onNoteClicked: (note: NoteModel) => void;
+  onDeleteNoteClicked: (note: NoteModel) => void;
 }
 
-const Note = ({ note, className }: NoteProps) => {
+const Note = ({
+  note,
+  onDeleteNoteClicked,
+  onNoteClicked,
+  className,
+}: NoteProps) => {
   const { title, text, createdAt, updatedAt } = note;
 
   let createdUpdatedText: string;
@@ -22,9 +32,21 @@ const Note = ({ note, className }: NoteProps) => {
   }
 
   return (
-    <Card className={`${styles.noteCard} ${className}`}>
+    <Card
+      className={`${styles.noteCard} ${className}`}
+      onClick={() => onNoteClicked(note)}
+    >
       <Card.Body className={styles.cardBody}>
-        <Card.Title>{title}</Card.Title>
+        <Card.Title className={stylesUtils.flexCenter}>
+          {title}
+          <MdDelete
+            className="text-muted ms-auto"
+            onClick={(e) => {
+              onDeleteNoteClicked(note);
+              e.stopPropagation();
+            }}
+          />
+        </Card.Title>
         <Card.Text className={styles.cardText}>{text}</Card.Text>
       </Card.Body>
       <Card.Footer className="text-muted">{createdUpdatedText}</Card.Footer>
